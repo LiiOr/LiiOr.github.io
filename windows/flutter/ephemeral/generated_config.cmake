e1e1907df1e577cda9bb64fc39a8744da6ac021a
LiiOr <laure.delamarre@hotmail.fr>
2023-07-09 14:29:56 +0200

    test commit apres mise en place workflow et changement de name repo

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "E:\\LiiOr\\PROG\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "E:\\LiiOr\\PROG\\pawpatrol\\flappypatrol" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=E:\\LiiOr\\PROG\\flutter"
+  "PROJECT_DIR=E:\\LiiOr\\PROG\\pawpatrol\\flappypatrol"
+  "FLUTTER_ROOT=E:\\LiiOr\\PROG\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=E:\\LiiOr\\PROG\\pawpatrol\\flappypatrol\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=E:\\LiiOr\\PROG\\pawpatrol\\flappypatrol"
+  "FLUTTER_TARGET=E:\\LiiOr\\PROG\\pawpatrol\\flappypatrol\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=E:\\LiiOr\\PROG\\pawpatrol\\flappypatrol\\.dart_tool\\package_config.json"
+)
