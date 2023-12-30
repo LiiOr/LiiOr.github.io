ff18a446e0feef997a9410bcc7a5136a1304b916
LiiOr <laure.delamarre@hotmail.fr>
2023-12-30 15:53:06 +0100

    added pong scores + keyboards controls

--- lib/games/pong_game.dart
+++ lib/games/pong_game.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:mylabs/globals.dart';
 import 'package:mylabs/games/scores.dart';
 
@@ -54,6 +55,13 @@ class _PongGameState extends State<PongGame> {
           ballX >= paddleX &&
           ballX <= paddleX + paddleWidth) {
         ballSpeedY = -ballSpeedY;
+        score++;
+        if (score > highScore) {
+          highScore = score;
+          var scoreboard =
+              GameScore(game: 'PONG', score: score, highScore: highScore);
+          scoreboard.setScore();
+        }
       }
     });
   }
@@ -69,11 +77,22 @@ class _PongGameState extends State<PongGame> {
     });
   }
 
-  
+  void movePaddle(double dx) {
+  setState(() {
+    paddleX += dx;
+    if (paddleX < 0) {
+      paddleX = 0;
+    } else if (paddleX > screenWidth - paddleWidth) {
+      paddleX = screenWidth - paddleWidth;
+    }
+  });
+}
+
   @override
   void dispose() {
-    gameLoopTimer?.cancel(); 
-    var scoreboard = GameScore(game: 'PONG', score: score, highScore: highScore);
+    gameLoopTimer?.cancel();
+    var scoreboard =
+        GameScore(game: 'PONG', score: score, highScore: highScore);
     scoreboard.setScore();
     super.dispose();
   }
@@ -84,37 +103,50 @@ class _PongGameState extends State<PongGame> {
       appBar: AppBar(
         title: const Text('P O N G'),
       ),
-      body: Column(
-        children: [
-          Expanded(
-            child: GestureDetector(
-              onHorizontalDragUpdate: onDragUpdate,
-              child: Stack(
-                children: [
-                  Positioned(
-                    left: ballX,
-                    top: ballY,
-                    child: Container(
-                      width: 20,
-                      height: 20,
-                      color: Colors.yellow,
+      body: RawKeyboardListener(
+        focusNode: FocusNode(),
+      autofocus: true,
+      onKey: (RawKeyEvent event) {
+        if (event is RawKeyDownEvent) {
+          if (event.logicalKey == LogicalKeyboardKey.arrowRight) {
+            movePaddle(10.0);
+          } else if (event.logicalKey == LogicalKeyboardKey.arrowLeft) {
+            movePaddle(-10.0);
+          }
+        }
+      },
+        child: Column(
+          children: [
+            Expanded(
+              child: GestureDetector(
+                onHorizontalDragUpdate: onDragUpdate,
+                child: Stack(
+                  children: [
+                    Positioned(
+                      left: ballX,
+                      top: ballY,
+                      child: Container(
+                        width: 20,
+                        height: 20,
+                        color: Colors.yellow,
+                      ),
                     ),
-                  ),
-                  Positioned(
-                    left: paddleX,
-                    top: paddleY,
-                    child: Container(
-                      width: paddleWidth,
-                      height: 20,
-                      color: Colors.pink[700],
+                    Positioned(
+                      left: paddleX,
+                      top: paddleY,
+                      child: Container(
+                        width: paddleWidth,
+                        height: 20,
+                        color: Colors.pink[700],
+                      ),
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
             ),
-          ),
-          GameScore(game: 'PONG', score: score, highScore: score)
-        ],
+            GameScore(game: 'PONG', score: score, highScore: score)
+          ],
+        ),
       ),
     );
   }
