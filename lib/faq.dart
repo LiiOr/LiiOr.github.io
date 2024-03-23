010f2be417b14c7636f07e51e440c3186a323673
LiiOr <laure.delamarre@hotmail.fr>
2024-03-23 10:20:16 +0100

    Fix pb repo portble

--- lib/faq.dart
+++ lib/faq.dart
@@ -5,7 +5,6 @@ class FAQ extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
     return Scaffold(
         appBar: AppBar(
           title: const Text('F A Q'),
