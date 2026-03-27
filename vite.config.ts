bb7c8b89ae3375f70f51ca415dd0f239bf41ad67
LiiOr <laure.delamarre@hotmail.fr>
2026-03-27 10:30:25 +0100

    Retouches icones

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
     react(),
     VitePWA({
       registerType: 'autoUpdate',
-      includeAssets: ['favicon.png', 'icon.png', 'favicon.svg'],
+      includeAssets: ['favicon.svg', '192.png', '512.png'],
       manifest: {
         id: '/',
         name: 'Main Verte',
@@ -22,25 +22,25 @@ export default defineConfig({
         scope: '/',
         icons: [
           {
-            src: '/icon.png',
+            src: '/192.png',
             sizes: '192x192',
             type: 'image/png',
             purpose: 'any'
           },
           {
-            src: '/icon.png',
+            src: '/512.png',
             sizes: '512x512',
             type: 'image/png',
             purpose: 'any'
           },
           {
-            src: '/icon.png',
+            src: '/192.png',
             sizes: '192x192',
             type: 'image/png',
             purpose: 'maskable'
           },
           {
-            src: '/icon.png',
+            src: '/512.png',
             sizes: '512x512',
             type: 'image/png',
             purpose: 'maskable'
